9fef4514fcd097cca85b875d2de891ca008e3c2c
Dalton Pettus <daltpettus@gmail.com>
2025-05-16 14:01:54 -0700

    completed example project

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import LikeButton from "./like-button";
 
 function Header({ title }) {
     return <h1>{title || 'Default title'}</h1>;
@@ -7,20 +7,13 @@ function Header({ title }) {
 function HomePage() {
     const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
 
-    const [likes, setLikes] = useState(0);
-
-    const handleClick = () => {
-        setLikes(() => likes + 1);
-    }
-
     return (
         <section>
             <Header title="Develop. Preview. Ship." />
             <ul>
                 {names.map((name) => <li key={name}>{name}</li>)}
             </ul>
-
-            <button onClick={handleClick}>Like ({likes})</button>
+            <LikeButton />
         </section>
     );
 }
